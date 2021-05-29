ed21436b09de359ce10f3068115889774ff27ccb
DavidChavess <davi.ch.fe@gmail.com>
2021-05-29 13:10:53 -0300

    starting project

---
+++ src/main/kotlin/com/ferreira/kotlinapi/controller/HelloWorldController.kt
@@ -0,0 +1,13 @@
+package com.ferreira.kotlinapi.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun hello() = "Hello world"
+}
---
+++ src/test/kotlin/com/ferreira/kotlinapi/KotlinApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ferreira.kotlinapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
